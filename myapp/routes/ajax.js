ad5d806348ea3366891e835d59748295e102e70c
chohyojin <patksungjune@gmail.com>
2017-07-14 14:47:07 +0900

    ww

--- myapp/routes/ajax.js
+++ myapp/routes/ajax.js
@@ -1,29 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+
 /* POST 호출 처리 */
 router.post('/', function(req, res, next) {
-	fs.readFile('user.js','utf8',function(error, data){
+	fs.readFile('user.js','utf8',function(error, data) {
 		var obj = JSON.parse(data);
-		console.log(obj);
-    	console.log('POST 방식으로 서버 호출됨');
+		//console.log(obj);
     	var msg = req.body.msg;
     	
-    	try{
-    	if(msg == obj[msg].user_id){
-    		res.json( obj[msg]);
+    	try {
+    		if (msg == obj[msg].user_id) {
+    		   res.json( obj[msg]);
+    	    }
+    	} catch (exception) {
+    		res.json({result:false});
     	}
     	
-    	}catch(exception)
-    	
-    	{
-    		res.json({result:false})
-    	}
-    	
-   		
-    	//res.send({result:true, msg:obj[msg].user_id, msg2:obj[msg].user_name});
-    	
-    	
+    	//res.send({result:true, msg:obj[msg].user_id, msg2:obj[msg].user_name});	
 	});
- });
+});
+
 module.exports = router;
--- myapp/user.js
+++ myapp/user.js
@@ -2,10 +2,13 @@
 		 "1" :{
 			"user_id" : "1", "user_name" : "박성준", "device" : "Android"},
 			
-
-			
 		 "2" :{
-		 	"user_id":"2", "user_name":"주용재", "device":"iOS"
-		}
+		 	"user_id" : "2", "user_name" : "주용재", "device" : "iOS"},
 
+		 "3" :{
+			"user_id" : "3", "user_name" : "마민지", "device" : "iOS"},
+			
+		 "4" :{
+			"user_id" : "4", "user_name" : "이승연", "device" : "iOS"}
+			
 }
